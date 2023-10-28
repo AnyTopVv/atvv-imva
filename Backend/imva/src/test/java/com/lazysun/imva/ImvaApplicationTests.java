4197aa5208f8c4cd5a1421b12ff3d3ac8288ca32
lazysun <848539845@qq.com>
2023-10-28 20:12:03 +0800

    backend init

---
+++ Backend/imva/src/test/java/com/lazysun/imva/ImvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazysun.imva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
